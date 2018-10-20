b04c4c0097588ce7666f037b98f5934f41f768c9
Galuh Pradipta <galuhpradipta95@gmail.com>
2018-10-20 23:53:46 +0700

    initial push

---
+++ deck_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("expected deck to have 52 cards, but only got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards , got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
